199bfa4789e783794fadffe78a2767ce311d608a
compactrex66 <113365941+compactrex66@users.noreply.github.com>
2024-04-11 16:50:00 +0200

    Delete Gra directory

--- Gra/src/Items/Armors/DragonScaleArmor.java
+++
@@ -1,9 +0,0 @@
-package Items.Armors;
-
-import Items.Armor;
-
-public class DragonScaleArmor extends Armor {
-    public DragonScaleArmor() {
-        super("Dragon Scale Armor", 450, 15, 18);
-    }
-}
--- Gra/src/Items/Armors/LeatherArmor.java
+++
@@ -1,9 +0,0 @@
-package Items.Armors;
-
-import Items.Armor;
-
-public class LeatherArmor extends Armor {
-    public LeatherArmor() {
-        super("Leather Armor", 120, 8, 9);
-    }
-}
